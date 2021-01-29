7a33a956eec40894c071b33e54a72aac81153983
shayan karami <46638608+iamshayankarami@users.noreply.github.com>
2021-01-29 23:00:53 +0330

    Update CellDataEdith.java

--- src/CellDataEdith.java
+++ src/CellDataEdith.java
@@ -1,22 +1,27 @@
-package GOL;
+package GameOfLife2_0;
 
-public class CellDataEdith {
+public class CellDataEdit {
 	private String indexData;
-	
-	public CellDataEdith(String inputData) {
+
+	public CellDataEdit(String inputData) {
 		indexData = inputData;
 	}
-	
+
 	public int GetCellX() {
 		return Integer.parseInt(indexData.substring(0, indexData.indexOf('|')));
 	}
-	
+
 	public int GetCellY() {
 		return Integer.parseInt(indexData.substring(indexData.indexOf('|')+1, indexData.length()).substring(0, indexData.substring(indexData.indexOf('|')+1, indexData.length()).indexOf('|')));
 	}
-	
+
 	public int GetCellColor() {
-		return Integer.parseInt(indexData.substring(indexData.indexOf('|')+1, indexData.length()).substring(indexData.substring(indexData.indexOf('|')+1, indexData.length()).indexOf('|')+1));
+		String procces = indexData.substring(indexData.indexOf('|')+1, indexData.length());
+		procces = procces.substring(procces.indexOf('|')+1, procces.length());
+		return Integer.parseInt(procces.substring(0, procces.indexOf('|')));
+	}
+	public char GetCellShape() {
+		return indexData.charAt(indexData.length()-1);
+
 	}
 }
-
